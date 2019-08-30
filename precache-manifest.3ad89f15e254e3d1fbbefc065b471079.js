2e03fa3c1674db788da27a228ed2a9d6f7fbbc82
Tom <tsechrist@outlook.com>
2019-08-29 22:54:53 -0500

    Updates

---
+++ precache-manifest.3ad89f15e254e3d1fbbefc065b471079.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3187f65ec3fca06ad5bb03e88105ada",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "452dd6c9b59333060b65",
+    "url": "/clicky-game/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "cd65778a53a5ab20ce46",
+    "url": "/clicky-game/static/js/2.33649e61.chunk.js"
+  },
+  {
+    "revision": "452dd6c9b59333060b65",
+    "url": "/clicky-game/static/js/main.a316c00c.chunk.js"
+  },
+  {
+    "revision": "79ea0d5502c3a6ac0f74",
+    "url": "/clicky-game/static/js/runtime~main.80bf01e5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicky-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
