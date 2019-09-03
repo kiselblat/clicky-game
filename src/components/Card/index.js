fe4f1fcb56f013b9b628512880cb8331f54b1b5d
Tom <tsechrist@outlook.com>
2019-09-02 20:34:41 -0500

    Edits name of click handler

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./style.css";
 
 const Card = props => (
-  <div className="card" onClick={ () => props.clickCount(props.id) }>
+  <div className="card" onClick={ () => props.clickHandler(props.id) }>
     <div className="picture-frame">
       <img alt={props.name} src={props.image} />
     </div>
